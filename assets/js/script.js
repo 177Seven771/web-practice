45d6c1dbbc744fbd23c0f82a24142b983d010ec5
177Seven771 <mackros228@gmail.com>
2026-06-01 13:19:55 +0300

    add hero section, header navigation and mobile burger menu with js logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,22 @@
-console.log("js work")
+const closeBtn = document.querySelector('.close-btn');
+const notificationBar = document.querySelector('.notification-bar');
+
+if (notificationBar) {
+  notificationBar.style.display = 'flex';
+}
+
+if (closeBtn && notificationBar) {
+  closeBtn.addEventListener('click', () => {
+    notificationBar.style.display = 'none';
+  });
+}
+
+const burgerMenu = document.querySelector('.burger-menu');
+const navMenu = document.querySelector('.nav-menu');
+
+if (burgerMenu && navMenu) {
+  burgerMenu.addEventListener('click', () => {
+    burgerMenu.classList.toggle('active');
+    navMenu.classList.toggle('active');
+  });
+}
